7f9a4194f07d35d882510d639d23d4b2848ed324
Ádamo Rocha <43767932+adamorocha@users.noreply.github.com>
2023-06-27 09:23:02 -0300

    Inicio do Projeto

---
+++ js/animacao.js
@@ -0,0 +1,37 @@
+function moverImagens() {
+    var imagens = document.getElementsByClassName('frutaAnimada');
+    var windowWidth = window.innerWidth;
+    var windowHeight = window.innerHeight;
+
+    for (var i = 0; i < imagens.length; i++) {
+        var imagem = imagens[i];
+        var x = Math.floor(Math.random() * (windowWidth - imagem.width));
+        var y = Math.floor(Math.random() * (windowHeight - imagem.height));
+
+        // Verifica se a imagem ultrapassa os limites da tela e ajusta a posição
+        if (x < 0) {
+            x = 0;
+        } else if (x + imagem.width > windowWidth) {
+            x = windowWidth - imagem.width;
+        }
+
+        if (y < 0) {
+            y = 0;
+        } else if (y + imagem.height > windowHeight) {
+            y = windowHeight - imagem.height;
+        }
+
+        anime({
+            targets: imagem,
+            translateX: x,
+            translateY: y,
+            duration: 100,
+            easing: 'easeInOutQuad'
+        });
+    }
+}
+
+        setInterval(moverImagens, 1500); // Movimenta as imagens a cada 2 segundos
+
+        // Inclua a biblioteca Anime.js no seu projeto para utilizar a função anime()
+        // Contruido com Auxilio do CHATGPT
